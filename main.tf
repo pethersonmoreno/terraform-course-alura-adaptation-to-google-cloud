065533c5cb9f0310b19e79bad7147d674bd50f06
Petherson Moreno <pethermoreno@gmail.com>
2020-07-11 15:17:43 -0300

    feat: created network and firewall rule  - Equal to create Security Group in AWS

--- main.tf
+++ main.tf
@@ -24,12 +24,34 @@ resource "google_compute_instance" "dev" {
   }
 
   network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
+    # network = "default"
+    network = google_compute_network.default.name
     access_config {
      // Include this section to give the VM an external ip address
     }
   }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+  # network = "default"
+
+  direction = "INGRESS"
+
+  source_ranges = ["187.57.0.19/32"]
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_network" "default" {
+  name  = "default-terraform-pet"
+  project = "terraform-pet"
+  routing_mode  = "REGIONAL"
+  description = "Default network for the project terraform-pet"
+  timeouts {}
 }
 
 output "ip" {
