73d2b03a36b8f9f6c8ddef2b7c129ae31591066c
Petherson Moreno <pethermoreno@gmail.com>
2020-07-11 18:18:50 -0300

    feat: added dev4 and dev5 machines as copy of machine dev to use in example

--- main.tf
+++ main.tf
@@ -53,6 +53,58 @@ resource "google_compute_network" "default" {
   description = "Default network for the project terraform-pet"
   timeouts {}
 }
+resource "google_compute_instance" "dev4" {
+  name         = "terraform-google-dev4"
+  machine_type = "f1-micro" # 1 CPU and 614 MB memory
+  zone         = "us-east1-c"
+
+  tags = ["terraform", "dev4"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20200701"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  network_interface {
+    # network = "default"
+    network = google_compute_network.default.name
+    access_config {
+     // Include this section to give the VM an external ip address
+    }
+  }
+}
+
+
+resource "google_compute_instance" "dev5" {
+  name         = "terraform-google-dev5"
+  machine_type = "f1-micro" # 1 CPU and 614 MB memory
+  zone         = "us-east1-c"
+
+  tags = ["terraform", "dev5"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20200701"
+    }
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  network_interface {
+    # network = "default"
+    network = google_compute_network.default.name
+    access_config {
+     // Include this section to give the VM an external ip address
+    }
+  }
+}
 
 resource "google_storage_bucket" "dev4" {
   name          = "pethersonmorenotesting-dev4"
